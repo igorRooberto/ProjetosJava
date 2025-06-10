4221d3d0d819071bed67e37b38a583d44454df1e
Igor Roberto <isilvalopp@gmail.com>
2025-06-10 15:06:20 -0300

    WhilePares

---
+++ whilePares.java
@@ -0,0 +1,17 @@
+public class whilePares {
+
+    public static void main(String[] args){
+        int i = 1;
+
+
+        while(i <= 20){
+
+            if(i % 2 == 0){
+                System.out.println("Números Pares : "+i);
+            }
+
+            i++;
+        }
+
+    }
+}
