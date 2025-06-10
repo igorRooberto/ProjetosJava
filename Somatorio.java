7b1623b0e40c5168c9a73dca4b8fd086fee6740e
Igor Roberto <isilvalopp@gmail.com>
2025-06-10 15:00:18 -0300

    Primeiro commit

---
+++ Somatorio.java
@@ -0,0 +1,19 @@
+public class Somatorio {
+
+    public static void main(String[] args){
+        int soma = 0;
+
+        for(int i = 0; i <= 100;i++){
+            soma += i;
+
+        }
+
+        System.out.println("A soma total é : "+soma);
+
+
+
+
+
+    }
+
+}
